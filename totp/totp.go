3c170f8f99f2a74c7c2d12c1278015953e4b64d9
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-11-20 09:27:24 -0800

    Add TOTP package
    Implemented basic TOTP checks on top of the HOTP package.
    
    This needs more tests and a refactor but I'm not sure the path to take
    to complete this yet. This is more about getting the minimal viable
    functionality established.

--- totp/totp.go
+++ totp/totp.go
@@ -3,9 +3,30 @@ package totp
 import "time"
 import "github.com/jonfriesen/otp"
 
+const defaultStep int64 = 30
+
 // Generate Generates an TOTP
 // Note: TOTP recommended length is 8 as per RFC 6238
 func Generate(secret string, t time.Time, length int) string {
-	tw := int(t.Unix() / 30)
+	tw := int(t.Unix() / defaultStep)
 	return hotp.Generate(secret, tw, length)
+}
+
+// Check validates an TOTP
+// accepts secret, time, length to generate the OTP's to validate
+// an incoming OTP, and how many times to increase the validation count
+func Check(secret string, t time.Time, length int, otp string) bool {
+	// This is the size on either side of the time that
+	// valid tokens will be accepted
+	windowStepSize := 2
+	windowStep := 30
+
+	// Iterate over the stepSize on both sides totalComparisons = stepSize * 2
+	for i := -1 * windowStepSize; i < windowStepSize; i++ {
+		nt := t.Add(time.Second * time.Duration(i*windowStep))
+		if Generate(secret, nt, length) == otp {
+			return true
+		}
+	}
+	return false
 }
--- totp/totp_test.go
+++ totp/totp_test.go
@@ -19,3 +19,18 @@ func TestGenerate(t *testing.T) {
 		t.Errorf("Expected %v but got %v", eOtp, rOtp)
 	}
 }
+
+func TestCheck(t *testing.T) {
+	tm := time.Date(1970, 1, 1, 0, 0, 59, 0, time.UTC)
+	eOtp := "94287082"
+
+	isValid := Check(defaultSecret, tm, defaultLength, eOtp)
+	if !isValid {
+		t.Errorf("Expected %v to be valid but was %v", eOtp, isValid)
+	}
+
+	isValid = Check(defaultSecret, tm, defaultLength, "12345678")
+	if isValid {
+		t.Errorf("Expected %v to be invalid but was %v", eOtp, isValid)
+	}
+}
